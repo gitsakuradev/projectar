d6cd6c7083f79c21bf4f1eb7a5d1a6df2a4f3878
gitsakuradev <synexisx@gmail.com>
2026-01-10 15:30:15 +0300

    Add routes for Gallery3D and Subjects pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Markers from './pages/Markers'
 import ARScanner from './pages/ARScanner'
+import Gallery3D from './pages/Gallery3D'
+import Subjects from './pages/Subjects'
 import './App.css'
 
 function App() {
@@ -9,8 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/subjects" element={<Subjects />} />
         <Route path="/markers" element={<Markers />} />
-        <Route path="/ar" element={<ARScanner />} />
+        <Route path="/ar/:subject?" element={<ARScanner />} />
+        <Route path="/gallery/:subject?" element={<Gallery3D />} />
       </Routes>
     </Router>
   )
